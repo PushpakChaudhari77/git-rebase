30972284a4cbdd432674a5daf890aa53204aa180
Pushpak77 <pushpakc357@gmail.com>
2026-03-10 17:18:17 +0530

    resolve conflicts and sysout added

--- hello.java
+++ hello.java
@@ -4,12 +4,16 @@ public class App {
 
         System.out.println("Hello World");
         System.out.println("Welcome to Java Programming");
+
         int a = 10;
         int b = 20;
 
         int sum = a + b;
 
         System.out.println("Sum is: " + sum);
+
+
+	 System.out.println("Java Programming");
 
     }
 }
